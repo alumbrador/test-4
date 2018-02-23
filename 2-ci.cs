3c2a61474bdf9694d2b1a80644b5a2d6a6e3cf54
alumbrador <muradnm@code.edu.az>
2018-02-23 21:01:32 +0400

    Murad

---
+++ 2-ci.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Eded daxil edin");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int result = _sade(eded);
+            if (result == 0)
+            {
+                Console.WriteLine("{0}sade eded deyil", eded);
+            }
+            else
+            {
+                Console.WriteLine("{0} sade ededdir", eded);
+            }
+            Console.Read();
+        }
+
+        static  int _sade(int eded)
+        {
+            int i;
+            for (i = 2; i <= eded - 1; i++)
+            {
+                if (eded % i == 0)
+                {
+                    return 0;
+                }
+            }
+            if (i == eded)
+            {
+                return 1;
+            }
+            return 0;
+
+
+        }
+
+    }
+}
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+   
